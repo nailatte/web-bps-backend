506a3f935c46d77bd5a73329e2df8d754c9fba70
nailatte <n240305@gmail.com>
2025-07-12 21:14:29 +0700

    feat: add sqlite db and render.yaml

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,11 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
+<<<<<<< HEAD
     'allowed_origins' => ['http://localhost:5174','https://web-bps-frontend-three.vercel.app/'],
+=======
+    'allowed_origins' => ['http://localhost:5174', 'https://web-bps-frontend-three.vercel.app/'],
+>>>>>>> c98ffa5 (Allow frontend domain in CORS config)
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
