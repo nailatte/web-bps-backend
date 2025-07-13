6a00f1c410030c7a5c1de867674b0a5241bdff71
nailatte <n240305@gmail.com>
2025-07-13 15:49:13 +0700

    change cors

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,8 @@ return [
     'paths' => ['api/*','sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'https://web-bps-frontend-three.vercel.app'
+        'https://web-bps-frontend-three.vercel.app',
+        'https://web-bps-frontend-git-main-nailas-projects-b55f08a2.vercel.app'
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
