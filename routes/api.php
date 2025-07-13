5871a05f12cad8127c55cd21fc9d72ff78587c4c
nailatte <n240305@gmail.com>
2025-07-13 14:05:48 +0700

    Test dbconnection

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PublikasiController;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,15 @@ use App\Http\Controllers\PublikasiController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/db-test', function () {
+    try {
+        DB::connection()->getPdo();
+        return response()->json(['message' => 'Database connected ✅']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 
